e7fc2bb7da363e84457b4aed02f46601893ad277
BacchusD <BacchusD@users.noreply.github.com>
2020-02-29 13:00:14 +0200

    BaseCommit
    PlayerModel. Run animation, Road move

---
+++ Assets/ControllersScript/RoadController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadController : MonoBehaviour {
+
+	public float speed = 5;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var move = new Vector3 (speed, 0, 0) * Time.deltaTime;
+		gameObject.transform.position += move;
+	}
+}
